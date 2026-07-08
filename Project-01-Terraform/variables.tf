1225b7ddc49b8c6a3a7680bab143f5a6ab184763
Archana Kumari <archanakumariit20@gmail.com>
2026-07-08 18:29:24 +0530

    Added Terraform Azure Resource Group project

---
+++ Project-01-Terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  description = "Azure Resource Group Name"
+  type        = string
+  default     = "rg-devops-demo"
+}
+
+variable "location" {
+  description = "Azure Region"
+  type        = string
+  default     = "Central India"
+}
