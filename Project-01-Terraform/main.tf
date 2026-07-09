232ba97f137f8f39921f85983b4eb7dff6c60320
Archana Kumari <archanakumariit20@gmail.com>
2026-07-09 12:12:16 +0530

    Refactored Terraform project to use variables

--- Project-01-Terraform/main.tf
+++ Project-01-Terraform/main.tf
@@ -4,21 +4,21 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-devops-demo"
+  name                = var.virtual_network_name
   address_space       = ["10.0.0.0/16"]
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "subnet-devops-demo"
+  name                 = var.subnet_name
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "archanadevops20260708"
+  name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.location
   account_tier             = "Standard"
--- Project-01-Terraform/variables.tf
+++ Project-01-Terraform/variables.tf
@@ -9,3 +9,21 @@ variable "location" {
   type        = string
   default     = "Central India"
 }
+
+variable "virtual_network_name" {
+  description = "Azure Virtual Network Name"
+  type        = string
+  default     = "vnet-devops-demo"
+}
+
+variable "subnet_name" {
+  description = "Azure Subnet Name"
+  type        = string
+  default     = "subnet-devops-demo"
+}
+
+variable "storage_account_name" {
+  description = "Azure Storage Account Name"
+  type        = string
+  default     = "archanadevops20260708"
+}
