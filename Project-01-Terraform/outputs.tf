be95c619137ede9616287d2bd168f067dc4a53ab
Archana Kumari <archanakumariit20@gmail.com>
2026-07-09 12:20:28 +0530

    Enhanced Terraform outputs

--- Project-01-Terraform/outputs.tf
+++ Project-01-Terraform/outputs.tf
@@ -1,7 +1,29 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+  description = "Name of the Azure Resource Group"
+  value       = azurerm_resource_group.rg.name
 }
 
-output "resource_group_location" {
-  value = azurerm_resource_group.rg.location
+output "virtual_network_name" {
+  description = "Name of the Azure Virtual Network"
+  value       = azurerm_virtual_network.vnet.name
+}
+
+output "subnet_name" {
+  description = "Name of the Azure Subnet"
+  value       = azurerm_subnet.subnet.name
+}
+
+output "storage_account_name" {
+  description = "Name of the Azure Storage Account"
+  value       = azurerm_storage_account.storage.name
+}
+
+output "resource_group_id" {
+  description = "Azure Resource Group ID"
+  value       = azurerm_resource_group.rg.id
+}
+
+output "storage_account_id" {
+  description = "Azure Storage Account ID"
+  value       = azurerm_storage_account.storage.id
 }
